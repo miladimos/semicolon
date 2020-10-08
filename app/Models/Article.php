f0898ddc9a3ba0dd80aaf7d6e46a250d62b09516
miladimos <milad.jafari6210@gmail.com>
2020-10-09 01:47:59 +0330

    install cviebrock/eloquent-sluggable package

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
 
     
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }

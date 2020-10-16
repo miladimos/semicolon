3dd7f87f0633e254207031619b6f3f2a41d519ba
miladimos <milad.jafari6210@gmail.com>
2020-10-16 12:38:47 +0330

    Add messages.php to fa,en lang

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'welcome' => 'Dear :user Welcome.',
+    'login' => 'Login',
+    'register' => 'Register',
+    'categories' => 'Categories',
+    'tags' => 'Tags',
+    'contactus' => 'Contact us',
+    'profile' => 'Profile',
+    'setting' => 'Setting',
+    'update' => 'Update',
+    'create' => 'Create',
+    'socials' => [
+        'site' => 'Site',
+        'instagram' => 'Instagram',
+        'telegram' => 'Telegram',
+        'youtube' => 'Youtube',
+        'aparat' => 'Aparat',
+        'linkedin' => 'Linkedin',
+        'email' => 'Email',
+        'tweeter' => 'Tweeter',
+        'github' => 'Github',
+        'gitlab' => 'Gitlab',
+        'atbox' => 'Atbox',
+    ]
+];
--- resources/lang/fa/messages.php
+++ resources/lang/fa/messages.php
@@ -1,5 +1,27 @@
 <?php
 
 return [
-
+    'welcome' => ' :user خوش آمدید.',
+    'login' => 'ورود',
+    'register' => 'ثبت نام',
+    'categories' => 'دسته بندی ها',
+    'tags' => 'برچسب ها',
+    'contactus' => 'تماس با ما',
+    'profile' => 'پروفایل',
+    'setting' => 'تنظیمات',
+    'update' => 'بروزرسانی',
+    'create' => 'ایجاد',
+    'socials' => [
+        'site' => 'سایت',
+        'instagram' => 'اینستاگرام',
+        'telegram' => 'تلگرام',
+        'youtube' => 'یوتیوب',
+        'aparat' => 'آپارات',
+        'linkedin' => 'لینکدین',
+        'email' => 'ایمیل',
+        'tweeter' => 'توییتر',
+        'github' => 'گیت هاب',
+        'gitlab' => 'گیت لب',
+        'atbox' => 'Atbox',
+    ]
 ];

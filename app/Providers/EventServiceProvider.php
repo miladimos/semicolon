ecd0b7bc478ea25be549cdf0e9ca78e3021bf520
miladimos <message.tome1331@gmail.com>
2020-10-21 18:39:56 +0330

    s

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+//        'App\Events\Site\User\UserRegisteredEvent' => [
+//            'App\Listeners\Site\User\UserRegisteredEvent\SendUserRegisteredNotificationListener',
+//        ],
     ];
 
     /**

261e1643dea7b6103f10f758ab7ea6b6406f6c0c
miladimos <milad.jafari6210@gmail.com>
2021-06-21 19:23:43 +0430

    add basic site template

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,5 @@
+@extends('site.layouts.master')
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('site.index');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {

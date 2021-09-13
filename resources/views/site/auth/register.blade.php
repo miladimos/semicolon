07903d46689b432fc3cb562853d76454f3f198fe
miladimos <milad.jafari6210@gmail.com>
2021-09-13 03:00:30 -0700

    add login routes

--- resources/views/site/auth/register.blade.php
+++ resources/views/site/auth/register.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                <h2><i class="fa fa-envelope-o"></i> Contact us</h2>
+                <h2><i class="fa fa-envelope-o"></i> Join us</h2>
             </div><!-- end col -->
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <ol class="breadcrumb">
--- routes/auth.php
+++ routes/auth.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Site\Auth\LoginController;
 use App\Http\Controllers\Site\Auth\RegisterController;
 
-Route::group(['middleware' => 'guest', 'as' => 'auth.'], function () {
-    Route::get('register', [RegisterController::class, 'showRegisterForm']);
+Route::group(['as' => 'auth.'], function () {
+    Route::get('register', [RegisterController::class, 'showRegisterForm'])->name('register.form');
+    Route::get('register', [RegisterController::class, 'register'])->name('register');
+
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login.form');
+    Route::get('login', [LoginController::class, 'login'])->name('login');
 });
